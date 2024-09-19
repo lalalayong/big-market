07054ea0f4abf069d34fd3442f778cc23c6303e0
JCG <2242238026@qq.com>
2024-09-19 16:00:35 +0800

    feat：基础层持久化数据

---
+++ big-market-infrastructure/src/main/java/com/jcg/infrastructure/persistent/dao/IStrategyAwardDao.java
@@ -0,0 +1,17 @@
+package com.jcg.infrastructure.persistent.dao;
+
+import com.jcg.infrastructure.persistent.po.StrategyAward;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+/**
+ * @author JCG
+ * @description 抽奖策略奖品明细配置 - 概率、规则 DAO
+ */
+@Mapper
+public interface IStrategyAwardDao {
+
+    List<StrategyAward> queryStrategyAwardList();
+
+}
---
+++ big-market-infrastructure/src/main/java/com/jcg/infrastructure/persistent/dao/IStrategyDao.java
@@ -0,0 +1,17 @@
+package com.jcg.infrastructure.persistent.dao;
+
+import com.jcg.infrastructure.persistent.po.Strategy;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+/**
+ * @author JCG
+ * @description 抽奖策略 DAO
+ */
+@Mapper
+public interface IStrategyDao {
+
+    List<Strategy> queryStrategyList();
+
+}
