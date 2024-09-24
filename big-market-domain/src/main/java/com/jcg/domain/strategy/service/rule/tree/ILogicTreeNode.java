7102e9fd3efba7a6f575dbadee3d309b2e027f13
JCG <2242238026@qq.com>
2024-09-24 11:40:06 +0800

    feat：抽奖规则树模型结构设计

---
+++ big-market-domain/src/main/java/com/jcg/domain/strategy/service/rule/tree/ILogicTreeNode.java
@@ -0,0 +1,13 @@
+package com.jcg.domain.strategy.service.rule.tree;
+
+
+import com.jcg.domain.strategy.service.rule.tree.factory.DefaultTreeFactory;
+
+/**
+ * @description 规则树接口
+ */
+public interface ILogicTreeNode {
+
+    DefaultTreeFactory.TreeActionEntity logic(String userId, Long strategyId, Integer awardId);
+
+}
