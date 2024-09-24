ee544078b2c3ba9fbeba456f39a5469a55f6eeab
JCG <2242238026@qq.com>
2024-09-24 11:40:06 +0800

    feat：抽奖规则树模型结构设计

---
+++ big-market-domain/src/main/java/com/jcg/domain/strategy/service/rule/tree/factory/engine/IDecisionTreeEngine.java
@@ -0,0 +1,13 @@
+package com.jcg.domain.strategy.service.rule.tree.factory.engine;
+
+
+import com.jcg.domain.strategy.service.rule.tree.factory.DefaultTreeFactory;
+
+/**
+ * @description 规则树组合接口
+ */
+public interface IDecisionTreeEngine {
+
+    DefaultTreeFactory.StrategyAwardData process(String userId, Long strategyId, Integer awardId);
+
+}
---
+++ big-market-domain/src/main/java/com/jcg/domain/strategy/service/rule/tree/impl/RuleStockLogicTreeNode.java
@@ -0,0 +1,23 @@
+package com.jcg.domain.strategy.service.rule.tree.impl;
+
+import com.jcg.domain.strategy.model.valobj.RuleLogicCheckTypeVO;
+import com.jcg.domain.strategy.service.rule.tree.ILogicTreeNode;
+import com.jcg.domain.strategy.service.rule.tree.factory.DefaultTreeFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+/**
+ * @description 库存扣减节点
+ */
+@Slf4j
+@Component("rule_stock")
+public class RuleStockLogicTreeNode implements ILogicTreeNode {
+
+    @Override
+    public DefaultTreeFactory.TreeActionEntity logic(String userId, Long strategyId, Integer awardId) {
+        return DefaultTreeFactory.TreeActionEntity.builder()
+                .ruleLogicCheckType(RuleLogicCheckTypeVO.TAKE_OVER)
+                .build();
+    }
+
+}
