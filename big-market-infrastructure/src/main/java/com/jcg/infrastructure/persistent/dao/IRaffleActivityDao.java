1bf9ceb8b6c54b5641afafc95b4bdee53da33489
JCG <2242238026@qq.com>
2024-09-29 10:22:14 +0800

    feat：引入分库分表路由组件

---
+++ big-market-infrastructure/src/main/java/com/jcg/infrastructure/persistent/dao/IRaffleActivityDao.java
@@ -0,0 +1,14 @@
+package com.jcg.infrastructure.persistent.dao;
+
+import com.jcg.infrastructure.persistent.po.RaffleActivity;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @description 抽奖活动表Dao
+ */
+@Mapper
+public interface IRaffleActivityDao {
+
+    RaffleActivity queryRaffleActivityByActivityId(Long activityId);
+
+}
