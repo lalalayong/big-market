ac20ad0e101c2b5980c95256beddb09dc98513a5
JCG <2242238026@qq.com>
2024-09-29 10:22:14 +0800

    feat：引入分库分表路由组件

---
+++ big-market-infrastructure/src/main/java/com/jcg/infrastructure/persistent/dao/IRaffleActivityAccountDao.java
@@ -0,0 +1,10 @@
+package com.jcg.infrastructure.persistent.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @description 抽奖活动账户表
+ */
+@Mapper
+public interface IRaffleActivityAccountDao {
+}
