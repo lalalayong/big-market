9724184c61a0f83e22714fe65aa2034b62af5418
JCG <2242238026@qq.com>
2024-09-30 18:21:55 +0800

    feat：引入MQ处理活动SKU库存一致性

--- big-market-domain/src/main/java/com/jcg/domain/activity/repository/IActivityRepository.java
+++ big-market-domain/src/main/java/com/jcg/domain/activity/repository/IActivityRepository.java
@@ -5,6 +5,9 @@ import com.jcg.domain.activity.model.aggregate.CreateOrderAggregate;
 import com.jcg.domain.activity.model.entity.ActivityCountEntity;
 import com.jcg.domain.activity.model.entity.ActivityEntity;
 import com.jcg.domain.activity.model.entity.ActivitySkuEntity;
+import com.jcg.domain.activity.model.valobj.ActivitySkuStockKeyVO;
+
+import java.util.Date;
 
 /**
  * @description 活动仓储接口
@@ -18,4 +21,19 @@ public interface IActivityRepository {
     ActivityCountEntity queryRaffleActivityCountByActivityCountId(Long activityCountId);
 
     void doSaveOrder(CreateOrderAggregate createOrderAggregate);
+
+    void cacheActivitySkuStockCount(String cacheKey, Integer stockCount);
+
+    boolean subtractionActivitySkuStock(Long sku, String cacheKey, Date endDateTime);
+
+    void activitySkuStockConsumeSendQueue(ActivitySkuStockKeyVO activitySkuStockKeyVO);
+
+    ActivitySkuStockKeyVO takeQueueValue();
+
+    void clearQueueValue();
+
+    void updateActivitySkuStock(Long sku);
+
+    void clearActivitySkuStock(Long sku);
+
 }
