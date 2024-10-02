afcada1c42b4a24e7506b8498195fd81f3865126
JCG <2242238026@qq.com>
2024-10-02 23:49:19 +0800

    feat：用户行为返利结算

--- big-market-app/src/test/java/com/jcg/test/domain/rebate/BehaviorRebateServiceTest.java
+++ big-market-app/src/test/java/com/jcg/test/domain/rebate/BehaviorRebateServiceTest.java
@@ -12,6 +12,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
 
 /**
  * @description 行为返利单测
@@ -25,16 +26,19 @@ public class BehaviorRebateServiceTest {
     private IBehaviorRebateService behaviorRebateService;
 
     @Test
-    public void test_createOrder() {
+    public void test_createOrder() throws InterruptedException {
         BehaviorEntity behaviorEntity = new BehaviorEntity();
         behaviorEntity.setUserId("JCG");
         behaviorEntity.setBehaviorTypeVO(BehaviorTypeVO.SIGN);
         // 重复的 OutBusinessNo 会报错唯一索引冲突，这也是保证幂等的手段，确保不会多记账
-        behaviorEntity.setOutBusinessNo("20241002");
+        // 一天只能签到1次
+        behaviorEntity.setOutBusinessNo("20240904");
 
         List<String> orderIds = behaviorRebateService.createOrder(behaviorEntity);
         log.info("请求参数：{}", JSON.toJSONString(behaviorEntity));
         log.info("测试结果：{}", JSON.toJSONString(orderIds));
+
+        new CountDownLatch(1).await();
     }
 
 }
--- big-market-infrastructure/src/main/java/com/jcg/infrastructure/persistent/dao/IRaffleActivityAccountDayDao.java
+++ big-market-infrastructure/src/main/java/com/jcg/infrastructure/persistent/dao/IRaffleActivityAccountDayDao.java
@@ -20,4 +20,5 @@ public interface IRaffleActivityAccountDayDao {
     @DBRouter
     Integer queryRaffleActivityAccountDayPartakeCount(RaffleActivityAccountDay raffleActivityAccountDay);
 
+    void addAccountQuota(RaffleActivityAccountDay raffleActivityAccountDay);
 }
