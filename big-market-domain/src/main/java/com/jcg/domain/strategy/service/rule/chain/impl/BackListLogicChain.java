0ddad2c27854773f7cb89a7f366994bf60324bbe
JCG <2242238026@qq.com>
2024-10-03 21:34:34 +0800

    fix：采用原型模型构建动态责任链

--- big-market-domain/src/main/java/com/jcg/domain/strategy/service/rule/chain/impl/BackListLogicChain.java
+++ big-market-domain/src/main/java/com/jcg/domain/strategy/service/rule/chain/impl/BackListLogicChain.java
@@ -5,6 +5,8 @@ import com.jcg.domain.strategy.service.rule.chain.AbstractLogicChain;
 import com.jcg.domain.strategy.service.rule.chain.factory.DefaultChainFactory;
 import com.jcg.types.common.Constants;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
@@ -14,6 +16,7 @@ import javax.annotation.Resource;
  */
 @Slf4j
 @Component("rule_blacklist")
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class BackListLogicChain extends AbstractLogicChain {
 
     @Resource
