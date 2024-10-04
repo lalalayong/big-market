1883d7696de653f8257dddc0b456877d3eb082d6
JCG <2242238026@qq.com>
2024-10-04 22:21:57 +0800

    fix：修改updateOrder()的redis锁、convert计算公式完善、查询未支付记录仅限支付类订单

--- big-market-domain/src/main/java/com/jcg/domain/activity/service/quota/AbstractRaffleActivityAccountQuota.java
+++ big-market-domain/src/main/java/com/jcg/domain/activity/service/quota/AbstractRaffleActivityAccountQuota.java
@@ -40,9 +40,11 @@ public abstract class AbstractRaffleActivityAccountQuota extends RaffleActivityA
             throw new AppException(ResponseCode.ILLEGAL_PARAMETER.getCode(), ResponseCode.ILLEGAL_PARAMETER.getInfo());
         }
 
-        // 2. 查询未支付订单「一个月以内的未支付订单」
-        UnpaidActivityOrderEntity unpaidCreditOrder =  activityRepository.queryUnpaidActivityOrder(skuRechargeEntity);
-        if (null != unpaidCreditOrder) return unpaidCreditOrder;
+        // 2. 查询未支付订单「一个月以内的未支付订单」& 支付类型查询，非支付的走兑换
+        if (OrderTradeTypeVO.credit_pay_trade.equals(skuRechargeEntity.getOrderTradeType())){
+            UnpaidActivityOrderEntity unpaidCreditOrder =  activityRepository.queryUnpaidActivityOrder(skuRechargeEntity);
+            if (null != unpaidCreditOrder) return unpaidCreditOrder;
+        }
 
         // 3. 查询基础信息
         // 3.1 通过sku查询活动信息
--- big-market-domain/src/main/java/com/jcg/domain/strategy/service/armory/StrategyArmoryDispatch.java
+++ big-market-domain/src/main/java/com/jcg/domain/strategy/service/armory/StrategyArmoryDispatch.java
@@ -12,7 +12,6 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.security.SecureRandom;
 import java.util.*;
 
@@ -72,6 +71,13 @@ public class StrategyArmoryDispatch implements IStrategyArmory, IStrategyDispatc
 
 
 
+    /**
+     * 计算公式；
+     * 1. 找到范围内最小的概率值，比如 0.1、0.02、0.003，需要找到的值是 0.003
+     * 2. 基于1找到的最小值，0.003 就可以计算出百分比、千分比的整数值。这里就是1000
+     * 3. 那么「概率 * 1000」分别占比100个、20个、3个，总计是123个
+     * 4. 后续的抽奖就用123作为随机数的范围值，生成的值100个都是0.1概率的奖品、20个是概率0.02的奖品、最后是3个是0.003的奖品。
+     */
     private void assembleLotteryStrategy(String key, List<StrategyAwardEntity> strategyAwardEntities) {
         // 1. 获取最小概率值
         BigDecimal minAwardRate = strategyAwardEntities.stream()
@@ -79,37 +85,84 @@ public class StrategyArmoryDispatch implements IStrategyArmory, IStrategyDispatc
                 .min(BigDecimal::compareTo)
                 .orElse(BigDecimal.ZERO);
 
-        // 2. 获取概率值总和
-        BigDecimal totalAwardRate = strategyAwardEntities.stream()
-                .map(StrategyAwardEntity::getAwardRate)
-                .reduce(BigDecimal.ZERO, BigDecimal::add);
+        // 2. 循环计算找到概率范围值
+        BigDecimal rateRange = BigDecimal.valueOf(convert(minAwardRate.doubleValue()));
 
-        // 3. 用 1 % 0.0001 获得概率范围，百分位、千分位、万分位
-        BigDecimal rateRange = totalAwardRate.divide(minAwardRate, 0, RoundingMode.CEILING);
-
-        // 4. 生成策略奖品概率查找表「这里指需要在list集合中，存放上对应的奖品占位即可，占位越多等于概率越高」
+        // 3. 生成策略奖品概率查找表「这里指需要在list集合中，存放上对应的奖品占位即可，占位越多等于概率越高」
         List<Integer> strategyAwardSearchRateTables = new ArrayList<>(rateRange.intValue());
         for (StrategyAwardEntity strategyAward : strategyAwardEntities) {
             Integer awardId = strategyAward.getAwardId();
             BigDecimal awardRate = strategyAward.getAwardRate();
             // 计算出每个概率值需要存放到查找表的数量，循环填充
-            for (int i = 0; i < rateRange.multiply(awardRate).setScale(0, RoundingMode.CEILING).intValue(); i++) {
+            for (int i = 0; i < rateRange.multiply(awardRate).intValue(); i++) {
                 strategyAwardSearchRateTables.add(awardId);
             }
         }
 
-        // 5. 对存储的奖品进行乱序操作
+        // 4. 对存储的奖品进行乱序操作
         Collections.shuffle(strategyAwardSearchRateTables);
 
-        // 6. 生成出Map集合，key值，对应的就是后续的概率值。通过概率来获得对应的奖品ID
+        // 5. 生成出Map集合，key值，对应的就是后续的概率值。通过概率来获得对应的奖品ID
         Map<Integer, Integer> shuffleStrategyAwardSearchRateTable = new LinkedHashMap<>();
         for (int i = 0; i < strategyAwardSearchRateTables.size(); i++) {
             shuffleStrategyAwardSearchRateTable.put(i, strategyAwardSearchRateTables.get(i));
         }
 
-        // 7. 存放到 Redis
+        // 6. 存放到 Redis
         repository.storeStrategyAwardSearchRateTable(key, shuffleStrategyAwardSearchRateTable.size(), shuffleStrategyAwardSearchRateTable);
     }
+
+    /**
+     * 转换计算，只根据小数位来计算。如【0.01返回100】、【0.009返回1000】、【0.0018返回10000】
+     */
+    private double convert(double min) {
+        if (0 == min) return 1D;
+
+        String minStr = String.valueOf(min);
+
+        // 小数点前
+        String beginVale = minStr.substring(0, minStr.indexOf("."));
+        int beginLength = 0;
+        if (Double.parseDouble(beginVale) > 0) {
+            beginLength = minStr.substring(0, minStr.indexOf(".")).length();
+        }
+
+        // 小数点后
+        String endValue = minStr.substring(minStr.indexOf(".") + 1);
+        int endLength = 0;
+        if (Double.parseDouble(endValue) > 0) {
+            endLength = minStr.substring(minStr.indexOf(".") + 1).length();
+        }
+
+        return Math.pow(10, beginLength + endLength);
+    }
+
+    private double convertOld1(double min) {
+        if (0 == min) return 1D;
+
+        double current = min;
+        double max = 1;
+        while (current % 1 != 0) {
+            current = current * 10;
+            max = max * 10;
+        }
+        return max;
+    }
+
+    private double convertOld2(double min) {
+        if (min == 0) return 1D;
+
+        String minStr = Double.toString(min);
+        int decimalPlaces = 0;
+
+        int decimalPointIndex = minStr.indexOf('.');
+        if (decimalPointIndex != -1) {
+            decimalPlaces = minStr.length() - decimalPointIndex - 1;
+        }
+
+        return Math.pow(10, decimalPlaces);
+    }
+
 
     @Override
     public Integer getRandomAwardId(Long strategyId) {
