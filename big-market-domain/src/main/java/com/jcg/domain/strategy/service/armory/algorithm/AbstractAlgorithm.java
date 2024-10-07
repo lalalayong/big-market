7a3cd66b4b5ffa98e914f9e9d934c7c491378b01
JCG <2242238026@qq.com>
2024-10-07 23:22:17 +0800

    feat：增强抽奖策略算法

---
+++ big-market-domain/src/main/java/com/jcg/domain/strategy/service/armory/algorithm/AbstractAlgorithm.java
@@ -0,0 +1,30 @@
+package com.jcg.domain.strategy.service.armory.algorithm;
+
+import com.jcg.domain.strategy.repository.IStrategyRepository;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.annotation.Resource;
+import java.security.SecureRandom;
+
+/**
+ * @description 抽奖算法抽象类
+ */
+public abstract class AbstractAlgorithm implements IAlgorithm {
+
+    @Resource
+    protected IStrategyRepository repository;
+
+    protected final SecureRandom secureRandom = new SecureRandom();
+
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public enum Algorithm {
+        O1("o1Algorithm"), OLogN("oLogNAlgorithm");
+
+        private String key;
+    }
+
+}
